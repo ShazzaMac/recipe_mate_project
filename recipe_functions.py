d2bdd2397dafad5236afab8b338134c9a3affac5
Sharon McAleer <splumridge01@qub.ac.uk>
2026-09-08 23:07:17 +0100

    defining some potential functions

--- recipe_functions.py
+++ recipe_functions.py
@@ -4,3 +4,22 @@ import foods #imports the food.py file so it can be used by the printIngredientN
 def printIngredientNames(ingredients): #our parameter here is ingredients
         for ingredient in foods.ingredients:
             print("Ingredient:", ingredient["name"])
+
+def calculateCalories(calories, grams): #function to calculate calories - calculated by the amount of calories whilst the grams is a percentage because all ingredients macros are based on 100g
+    result = calories * grams / 100
+    return result            
+
+def convertToMetric(amount, unit): #function to convert between metric and imperial units - requires amount and unit to perform calculation 
+     result = amount/ unit
+     return result            
+
+
+def scaleRecipe(amount, servings):
+      result = amount/ servings
+      return result  
+
+def calculateMacros():
+      return
+
+def displayRecipe(recipe):
+      return
