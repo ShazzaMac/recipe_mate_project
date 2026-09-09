c2245e2590e9f5f0a7789f0022304a5c6509fbfc
Sharon McAleer <splumridge01@qub.ac.uk>
2026-09-09 10:21:55 +0100

    small changes

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import recipe_functions
 import foods
 
 recipe_functions.printIngredientNames(foods.ingredients) #prints the ingredients names
-    
+
+print(recipe_functions.calculateCalories(300, 150))    
--- recipe_functions.py
+++ recipe_functions.py
@@ -5,8 +5,8 @@ def printIngredientNames(ingredients): #our parameter here is ingredients
         for ingredient in foods.ingredients:
             print("Ingredient:", ingredient["name"])
 
-def calculateCalories(calories, grams): #function to calculate calories - calculated by the amount of calories whilst the grams is a percentage because all ingredients macros are based on 100g
-    result = calories * grams / 100
+def calculateCalories(calories_per_100g, grams): #function to calculate calories - calculated by the amount of calories whilst the grams is a percentage because all ingredients macros are based on 100g
+    result = calories_per_100g * grams / 100
     return result            
 
 def convertToMetric(amount, unit): #function to convert between metric and imperial units - requires amount and unit to perform calculation 
