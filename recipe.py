f7f0b6d63c5cbf7401a82bd7a557b36638aaf420
Sharon McAleer <splumridge01@qub.ac.uk>
2026-09-12 22:13:00 +0100

    additional work on the recipe object and class including methods

--- recipe.py
+++ recipe.py
@@ -1,15 +1,37 @@
 #this file contains the recipe class
+import foods
 
 class Recipe:
-    def __init__(self, name, ingredients,method ):#initialising the object
-        self.name = name,
-        self.ingredients = ingredients,
-        self.method = method,
+    def __init__(self, name, ingredients, servings ):#initialising the object
+        self.name = name
+        self.ingredients = ingredients
+        self.servings = servings
+
+    def display(self):
+        print("Recipe:", self.name)
+
+    def scale_recipe(self, new_serving):
+        self.servings = new_serving
+
+    def display_ingredients(self):
+        for ingredient in self.ingredients:
+            print(ingredient)
+
 
 #below are the recipe objects
-
-recipe1 = Recipe( "Mexican chicken + Rice")
-recipe2 = Recipe("Air fried chicken + potatoes")
-recipe3 = Recipe("Chicken fajitas")
+recipe1 = Recipe( "Mexican chicken + Rice", foods.ingredients, 4) #by importing foods i can import the ingedients without having to re-write them 
+#recipe2 = Recipe("Air fried chicken + potatoes", foods.ingredients)
+#recipe3 = Recipe("Chicken fajitas", foods.ingredients)
 
 print(recipe1.name)
+#print(recipe2.name)
+#print(recipe3.name)
+
+recipe1.display()
+#recipe2.display()
+#recipe3.display()
+
+recipe1.scale_recipe(8)
+print(recipe1.servings)
+
+recipe1.display_ingredients()
